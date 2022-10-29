6e331697010afe1850a1ad1d8df979434cf7fd76
Higo Eduardo <higoedu@gmail.com>
2022-10-29 14:07:15 -0300

    comentário na classe principal

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,4 +2,10 @@ public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Seja bem vindo ao curso de java");
 	}
+	/*
+	pacotes
+	classe
+	métodos
+	variáveis
+	*/
 }
