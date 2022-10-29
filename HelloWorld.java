36c5c3fcb5d88ba04c567a3adcab9de69705931c
Higo Eduardo <higoedu@gmail.com>
2022-10-29 19:56:01 -0300

    tipo de variável

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,13 @@
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("Seja bem vindo ao curso de java");
+
+		/*
+		tipo inteiro - int
+		variável inteira - numeroInteiro
+		valor da variável - 10
+		*/
+		int numeroInteiro = 10;
 	}
 	/*
 	pacotes
