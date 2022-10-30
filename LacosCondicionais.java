e15220aa18140861478a2b950e55e1e964a32f2d
Higo Eduardo <higoedu@gmail.com>
2022-10-30 12:41:12 -0300

    comentário while

--- LacosCondicionais.java
+++ LacosCondicionais.java
@@ -6,9 +6,17 @@ public class LacosCondicionais{
 		executa bloco de instrução, enquanto a instrução for verdadeira
 		estrutura de repetição, iteração, looping
 		*/
+		/*
+		enquanto contador for menor e igual a 9
+		execute o que estiver dentro do bloco de instrução
+		*/
 		while(contador <= 9){
+			/*
 			contador = contador + 1;
-			System.out.println("looping");
+			outra maneira de fazer o incremento
+			*/
+			contador++;
+			System.out.println("looping while");
 		}
 	}
 }
