18d53ca35505e5c54b0aed8369811ed34b442fe4
Higo Eduardo <higoedu@gmail.com>
2022-10-30 10:56:10 -0300

    classe tipos primitivos

---
+++ TiposPrimitivos.java
@@ -0,0 +1,28 @@
+public class TiposPrimitivos{
+	public static void main(String[] args){
+		boolean pergunta = true;
+
+		char letra = 'H';
+
+		byte numeroB = 100;
+		
+		short numeroS = 2000;
+
+		int numeroI = 10000;
+
+		long numeroL = 250L;
+
+		float numeroF = 64.87f;
+
+		double numeroD = 540.11;
+
+		System.out.println("Pergunta: " + pergunta +
+							"\nletra: " + letra +
+							"\nnumero: " + numeroB +
+							"\nnumero: " + numeroS +
+							"\nnumero: " + numeroI +
+							"\nnumero: " + numeroL +
+							"\nnumero: " + numeroF +
+							"\nnumero: " + numeroD);
+	}
+}
