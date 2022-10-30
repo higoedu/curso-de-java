1f41b35b7402b646b386cafb71071924530c0387
Higo Eduardo <higoedu@gmail.com>
2022-10-30 08:38:08 -0300

    distancia som

--- Constante.java
+++ Constante.java
@@ -16,5 +16,19 @@ public class Constante{
 		numero = entrada.nextDouble();
 
 		System.out.println("Resultado: " + numero * CONSTANTE);
+
+		//Distância que o som vai percorre em um determinado espaço de tempo
+		final double velocidadeSom = 340.28;//m/s
+
+		System.out.println("Digite o espaco de tempo");
+		int tempo = entrada.nextInt();
+
+		System.out.println("A distancia e de: " +
+							tempo * velocidadeSom +
+							" metros percorridos");
+
+		System.out.println("A distancia em Km de: " +
+							(tempo * velocidadeSom) / 1000 +
+							" Km percorridos");
 	}
 }
