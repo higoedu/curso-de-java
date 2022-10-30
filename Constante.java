b57510c5e9084871b26d9556d16e48b32e2ff6ba
Higo Eduardo <higoedu@gmail.com>
2022-10-30 08:19:23 -0300

    comentário constante

--- Constante.java
+++ Constante.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 public class Constante{
 	public static void main(String[] args){
 		Scanner entrada = new Scanner(System.in);
+		/*
+		constante:
+		- em tempo de execução não será alterada
+		- tem endereço de memória protegido
+		- nunca será desolocada
+		- declaramos um vez, utilizamos quantas vezes for preciso
+		*/
 		final double CONSTANTE = 10;
 		double numero;
 
