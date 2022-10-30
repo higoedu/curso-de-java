31aa0dd84b3544cff1d204747c6aaadac851973a
Higo Eduardo <higoedu@gmail.com>
2022-10-30 11:45:29 -0300

    classe laços condicionais

---
+++ LacosCondicionais.java
@@ -0,0 +1,10 @@
+public class LacosCondicionais{
+	public static void main(String[] args){
+		int contador = 0;
+
+		while(contador <= 9){
+			contador = contador + 1;
+			System.out.println("looping");
+		}
+	}
+}
