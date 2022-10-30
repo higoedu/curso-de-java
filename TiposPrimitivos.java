eb87fa29f259ff81308d511b139b46c17457daab
Higo Eduardo <higoedu@gmail.com>
2022-10-30 11:12:44 -0300

    comentário tipos primitivos

--- TiposPrimitivos.java
+++ TiposPrimitivos.java
@@ -1,19 +1,27 @@
 public class TiposPrimitivos{
 	public static void main(String[] args){
+		//ocupa 1 byte, 8 bits
 		boolean pergunta = true;
 
+		//ocupa 2 byte, 16 bits
 		char letra = 'H';
 
+		//ocupa 1 byte, 8 bits
 		byte numeroB = 100;
 		
+		//ocupa 2 byte, 16 bits
 		short numeroS = 2000;
 
+		//ocupa 4 byte, 32 bits
 		int numeroI = 10000;
 
+		//ocupa 8 byte, 64 bits
 		long numeroL = 250L;
 
+		//ocupa 4 byte, 32 bits
 		float numeroF = 64.87f;
 
+		//ocupa 8 byte, 64 bits
 		double numeroD = 540.11;
 
 		System.out.println("Pergunta: " + pergunta +
