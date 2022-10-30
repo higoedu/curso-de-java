30a8a7be07f24ad99d613d13f9c223e0bb77b8a8
Higo Eduardo <higoedu@gmail.com>
2022-10-30 11:53:25 -0300

    comentário laços condicionais

--- LacosCondicionais.java
+++ LacosCondicionais.java
@@ -2,6 +2,10 @@ public class LacosCondicionais{
 	public static void main(String[] args){
 		int contador = 0;
 
+		/*
+		executa bloco de instrução, enquanto a instrução for verdadeira
+		estrutura de repetição, iteração, looping
+		*/
 		while(contador <= 9){
 			contador = contador + 1;
 			System.out.println("looping");
