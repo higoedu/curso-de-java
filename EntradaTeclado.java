341631d7c23e9b7b5cdc44346b98b874ee6db6e5
Higo Eduardo <higoedu@gmail.com>
2022-10-29 22:39:56 -0300

    classe entrada teclado

--- EntradaTeclado.java
+++ EntradaTeclado.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class EntradaTeclado{
 	public static void main(String[] args){
 		Scanner entrada = new Scanner(System.in);
+
 		System.out.println("Digite o nome");
 		System.out.println( entrada.nextLine() );
 	}
