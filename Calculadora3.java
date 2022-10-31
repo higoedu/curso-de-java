73e97d57887fd9884143cb0aea9c85009959494b
Higo Eduardo <higoedu@gmail.com>
2022-10-30 22:04:13 -0300

    comentário calculadora3

--- Calculadora3.java
+++ Calculadora3.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Calculadora3{
 	public static void main(String[] args){
 		Scanner entrada = new Scanner(System.in);
+		//onde vamos armazenar a operação a realizar
 		int x = 0;
 		double numero1, numero2, resultado;
 		numero1 = numero2 = resultado = 0;
@@ -17,6 +18,7 @@ public class Calculadora3{
 
 		x = entrada.nextInt();
 
+		//verifica se o usuário deseja sair do programa
 		if(x != 0){
 			System.out.println("Digite o 1 numero");
 			numero1 = entrada.nextDouble();
@@ -24,18 +26,22 @@ public class Calculadora3{
 			System.out.println("Digite o 2 numero");
 			numero2 = entrada.nextDouble();
 
+			//somar
 			if(x == 1){
 				resultado = numero1 + numero2;
 			}
 			else
+				//subtrair
 				if(x == 2){
 					resultado = numero1 - numero2;
 				}
 				else
+					//multiplicar
 					if(x == 3){
 						resultado = numero1 * numero2;
 					}
 					else
+						//dividir
 						if(x == 4){
 							resultado = numero1 / numero2;
 						}
