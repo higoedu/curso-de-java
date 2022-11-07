29860e3dc38f1d1d6993c31b4a46bc49a7c3891a
Higo Eduardo <higoedu@gmail.com>
2022-11-07 20:51:15 -0300

    determinado intervalo

--- NumerosAleatorios.java
+++ NumerosAleatorios.java
@@ -5,5 +5,13 @@ public class NumerosAleatorios{
 		Random numero = new Random();
 		//invocamos o método nextInt()
 		System.out.println(numero.nextInt());
+		/*
+		gerar número em um determinado intervalo
+		temos que passar o valor máximo desse intervalo
+		entre 0 e 5
+		*/
+		System.out.println(numero.nextInt(6));
+		
+
 	}
 }
