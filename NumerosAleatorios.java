1165604e7c17ab05574e973d20dbaa2767536b40
Higo Eduardo <higoedu@gmail.com>
2022-11-07 20:55:56 -0300

    looging for

--- NumerosAleatorios.java
+++ NumerosAleatorios.java
@@ -3,8 +3,10 @@ public class NumerosAleatorios{
 	public static void main(String[] args){
 		//atribui a instância Random à variável numero
 		Random numero = new Random();
+		
 		//invocamos o método nextInt()
 		System.out.println(numero.nextInt());
+		
 		/*
 		gerar número em um determinado intervalo
 		temos que passar o valor máximo desse intervalo
@@ -12,6 +14,12 @@ public class NumerosAleatorios{
 		*/
 		System.out.println(numero.nextInt(6));
 		
-
+		/*
+		quando for impresso o número 0, será impresso o número 1
+		quando for impresso o número 5, será impresso o número 6
+		*/
+		for(int i = 0; i < 20; i++)
+			System.out.println(numero.nextInt(6)+1);
+		
 	}
 }
