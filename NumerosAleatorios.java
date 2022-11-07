e4cfa4821db9071f8f888542f5c1a57275ef0ab6
Higo Eduardo <higoedu@gmail.com>
2022-11-07 20:27:43 -0300

    comentário número aleatório

--- NumerosAleatorios.java
+++ NumerosAleatorios.java
@@ -1,7 +1,9 @@
 import java.util.Random;
 public class NumerosAleatorios{
 	public static void main(String[] args){
+		//atribui a instância Random à variável numero
 		Random numero = new Random();
+		//invocamos o método nextInt()
 		System.out.println(numero.nextInt());
 	}
 }
