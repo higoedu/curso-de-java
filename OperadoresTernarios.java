400db1e80e6bb74024278f740ac48f662fe80dce
Higo Eduardo <higoedu@gmail.com>
2022-11-07 21:37:59 -0300

    comentário operadores ternários

--- OperadoresTernarios.java
+++ OperadoresTernarios.java
@@ -5,6 +5,11 @@ public class OperadoresTernarios{
 		a = 5;
 		b = 5;
 
+		/*
+		a > b - é a condição
+		se a condição for verdadeira, a variável valor recebe verdadeiro
+		se a condição for falsa, a variável valor recebe falso
+		*/
 		valor = a > b ? "verdadeiro" : "falso";
 
 		System.out.println(valor);
