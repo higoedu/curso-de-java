e1a3d6ebc52ce08d1d3d3c6f3a06574fe568841e
Higo Eduardo <higoedu@gmail.com>
2022-11-07 21:27:40 -0300

    classe operadores ternarios

---
+++ OperadoresTernarios.java
@@ -0,0 +1,12 @@
+public class OperadoresTernarios{
+	public static void main(String[] args){
+		int a, b;
+		String valor;
+		a = 5;
+		b = 5;
+
+		valor = a > b ? "verdadeiro" : "falso";
+
+		System.out.println(valor);
+	}
+}
