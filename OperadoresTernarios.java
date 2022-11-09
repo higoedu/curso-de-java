62c2855610c47fad2926651b9c61607224a001b0
Higo Eduardo <higoedu@gmail.com>
2022-11-08 21:41:31 -0300

    exemplo operador ternário

--- OperadoresTernarios.java
+++ OperadoresTernarios.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class OperadoresTernarios{
 	public static void main(String[] args){
 		int a, b;
@@ -13,5 +14,23 @@ public class OperadoresTernarios{
 		valor = a > b ? "verdadeiro" : "falso";
 
 		System.out.println(valor);
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Qual o seu dia da semana predileto");
+		System.out.println("Digite 1 para segunda e 7 para domingo");
+		int numero = entrada.nextInt();
+
+		String dia = (numero == 1) ? "segunda" :
+					 (numero == 2) ? "terca" :
+					 (numero == 3) ? "quarta" :
+					 (numero == 4) ? "quinta" :
+					 (numero == 5) ? "sexta" :
+					 (numero == 6) ? "sabado" :
+					 (numero == 7) ? "domingo" :
+					 				 "dia invalido";
+
+		System.out.println("O dia foi: " + dia);
+					 
 	}
 }
