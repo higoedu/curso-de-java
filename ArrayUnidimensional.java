3d119df189ce9be7432cd3803e0833dca399c5d8
Higo Eduardo <higoedu@gmail.com>
2022-11-09 21:31:29 -0300

    classe array unidimensional

---
+++ ArrayUnidimensional.java
@@ -0,0 +1,10 @@
+public class ArrayUnidimensional{
+	public static void main(String[] args){
+		int[] array;
+		array = new int[10];
+
+		for(int i = 0; i < 10; i++){
+			System.out.printf("%5d%7d\n", i, array[i]);
+		}
+	}
+}
