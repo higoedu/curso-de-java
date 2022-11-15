9ec1f7ddc5c561c33336f0e786158ebb0db42675
Higo <higoedu@gmail.com>
2022-11-15 11:11:46 -0300

    classe atividade array 1
    classe atividade array 1

--- AtividadeArray1.java
+++ AtividadeArray1.java
@@ -1,24 +1,16 @@
 import java.util.Arrays;
 import java.util.Scanner;
+public class AtividadeArray1{
+	public static void main(String[] args){
+		Scanner entrada = new Scanner(System.in);
+		int[] valores = new int[7];
 
-public class AtividadeArray1 {
-    public static void main(String[] args) {
-        /*
-        Peça para o usuário informar 7 números e
-        armazene-os em um array. Na sequência:
+		for(int j = 0; j < 7; j++) {
+			System.out.printf("Digite o numero da posicao [%d]:", j);
+			valores[j] = entrada.nextInt();
+		}
 
-        1) Some todos os números e exiba na tela
-        2) Multiplique o índice pelo seu valor
-        3) Calcule a média aritmética
-        */
-        Scanner entrada = new Scanner(System.in);
-        int[] valores = new int[7];
+		System.out.println(Arrays.toString(valores));
 
-        for (int i = 0; i < 7; i++){
-            System.out.printf("Digite o numero da posicao [%d]: ", i);
-            valores[i] = entrada.nextInt();
-        }
-
-        System.out.println(Arrays.toString(valores));
-    }
+	}
 }
