fe2193fae5155cedda0cb2f17df8bb4b2152989f
Higo <higoedu@gmail.com>
2022-11-30 19:31:39 -0300

    adicionou contrutor padrão
    adicionou contrutor padrão

--- ObjetoConstrutor.java
+++ ObjetoConstrutor.java
@@ -1,5 +1,15 @@
 class Janela{
 	int largura, altura;
+	boolean b;
+	float f;
+	double d;
+	//java inicializa as variáveis com seus valores padrões
+	Janela(){
+		System.out.println("int = " + largura);
+		System.out.println("boolean = " + b);
+		System.out.println("float = " + f);
+		System.out.println("double = " + d);
+	}
 	Janela(int l, int a){
 		largura = l;
 		altura = a;
@@ -11,7 +21,8 @@ class Janela{
 
 public class ObjetoConstrutor {
 	public static void main(String[] args){
-		Janela j = new Janela(250, 300);
-		j.imprimir();
+		Janela j1 = new Janela();
+		Janela j2 = new Janela(250, 300);
+		j2.imprimir();
 	}
 }
