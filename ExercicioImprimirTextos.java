f9c9aab4f1c1789845bb9d59f28347d0b5f98447
Higo <higoedu@gmail.com>
2022-12-14 21:21:08 -0300

    classe exercício imprimir textos
    classe exercício imprimir textos

---
+++ ExercicioImprimirTextos.java
@@ -0,0 +1,14 @@
+/*
+Faça um programa que imprima o texto "Ola mundo" para o usuário.
+Em seguida, imprima na tela o número 522 e então uma linha em branco.
+Depois de imprimir a linha em branco, escreva na tela a seguinte frase:
+"Seja bem-vindo a lista de exercícios resolvidos"
+*/
+public class ExercicioImprimirTextos {
+	public static void main(String[] args){
+		System.out.println("Ola mundo");
+		System.out.println(522);
+		System.out.println();
+		System.out.println("Seja bem-vindo a lista de exercícios resolvidos");
+	}
+}
